b4161977c9a811b49067efa9998d251fc0fda703
jjliewie <rheej@ismanila.org>
2021-09-27 14:00:45 +0800

    adding my files

---
+++ compute.py
@@ -0,0 +1,103 @@
+from mmap import PROT_READ
+import pandas as pd
+from math import sqrt
+import xlrd
+import numpy as np
+
+def euclidean_distance(x,y):
+  a = np.array(x)
+  b = np.array(y)
+  return np.linalg.norm(a-b)
+
+def cosine_similarity(v1,v2):
+    sumxx = 0
+    sumxy = 0
+    sumyy = 0
+    for i in range(len(v1)):
+        x = v1[i]
+        y = v2[i]
+        sumxx += (x*x)
+        sumyy += (y*y)
+        sumxy += (x*y)
+    
+    if sumxy  == 0 or (sumxx*sumyy) == 0:
+        return 1
+    return sumxy/sqrt(sumxx*sumyy)
+
+def calculate(text):
+    print('******************')
+    k = []
+    for i in text:
+        k.append(int(i))
+    print(k)
+    print(set(k))
+    if set(k) == {0}:
+        return "there are no jobs that fit your lackluster personality"
+
+    data = pd.read_excel(r'use.xlsx') 
+    jobs = []
+    for row in data:    
+        jobs.append(row)
+    jobs.pop(0)
+
+    book = xlrd.open_workbook('use.xlsx') 
+    sheet = book.sheet_by_name('Sheet1')
+    bayes = book.sheet_by_name('Sheet2')
+    da = [[sheet.cell_value(c, r) for c in range(1, sheet.nrows)] for r in range(1, sheet.ncols)]
+    la = [[bayes.cell_value(c, r) for c in range(1, bayes.nrows)] for r in range(1, bayes.ncols)]
+
+    print(da)
+    print(la)
+
+    s = 0
+    for t in la:
+        s += t[0]
+    print(s)
+
+    realistic = []
+    for h in la:
+        realistic.append(h[0]/s)
+    print('_______')
+    print(realistic)
+    print('_______')
+
+    d = []
+    for i in range(len(jobs)):
+        d.append((jobs[i], da[i]))
+    print(d)
+
+    compare = {}
+    for i in d:
+        compare[i[0]] = cosine_similarity(i[1], k)
+        # compare[i[0]] = euclidean_distance(i[1], k)
+    print('___*___')
+    print(compare)
+    print('___*___')
+
+    maximum = [0, 0]
+    for j, t in compare.items():
+        if t > maximum[1]:
+            maximum[1] = t
+            maximum[0] = j
+        print(str(j)+ " : " + str(t))
+
+    # print("the job that fits your personality best is: " + str(maximum[0]))
+
+    realistic_maximum = [0,0]
+    cnt = 0
+    for j, t in compare.items():
+        if (t * realistic[cnt]) > realistic_maximum[1]:
+            realistic_maximum[1] = (t * realistic[cnt])
+            realistic_maximum[0] = j
+        cnt += 1
+
+    # print("the job that you can most realistically atain is: " + str(realistic_maximum[0]))
+
+    if realistic_maximum[0] == 0:
+        return "there are no jobs that fit your lackluster personality"
+
+    return "the job that fits your personality best is: " + str(maximum[0]) + "\nthe job that fits your personality that you can most realistically attain is: " + str(realistic_maximum[0])
+
+# can i just say that writing this much code took me like 2 hours :(
+
+print(calculate('0010010010'))
---
+++ controller.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, request
+from compute import calculate
+
+app = Flask(__name__)
+
+class Question:
+    q_id = -1
+    question = ""
+    yes = ""
+    no = ""
+    qtype = ""
+    oneOption = -1
+
+    def __init__(self, q_id, question, yes, no, oneOption, qtype):
+        self.q_id = q_id
+        self.question = question
+        self.yes = yes
+        self.no = no
+        self.oneOption = oneOption
+        self.qtype = qtype
+
+    def pertains(self):
+        if self.oneOption == 1:
+            return self.yes
+        elif self.oneOption == 2:
+            return self.no
+
+q1 = Question(1, "Do you like to read?", "Yes", "No", 1, 'a')
+q2 = Question(2, "Do you like to run?", "Yes", "No", 1, 'f')
+q3 = Question(3, "Do you consider yourself a considerate person?", "Yes", "No", 1, 'b')
+q4 = Question(4, "Do you like children", "Yes", "No", 1, 'd')
+q5 = Question(5, "Do you think you are a hard working person?", "Yes", "No", 1, 'j')
+q6 = Question(6, "Do you care about the environment?", "Yes", "No", 1, 'i')
+q7 = Question(7, "Do you consider yourself artistic", "Yes", "No", 1, 'e')
+q8 = Question(8, "Do you like taking photos?", "Yes", "No", 1, 'h')
+q9 = Question(9, "Do you like to study", "Yes", "No", 1, 'g')
+
+questions_list = [q1, q2, q3, q4, q5, q6, q7, q8, q9]
+
+@app.route("/quiz")
+def quiz():
+    return render_template("quiz.html", questions_list = questions_list)
+
+@app.route("/submitquiz", methods=['POST', 'GET'])
+def submit():
+
+    points = {}
+    types = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+    for i in types:
+        points[i] = 0
+
+    for question in questions_list:
+        question_id = str(question.q_id)
+        question_type = str(question.qtype)
+        selected = request.form[question_id]
+        count = question.pertains()
+        if selected == count:
+            points[question_type] = 1
+    use = ""
+    for k, t in points.items():
+        use += str(t)
+    return calculate(use)
+
+    # value = request.form['option']
+    # return (value)
+
+if __name__ == "__main__":
+    app.run(debug=True)
